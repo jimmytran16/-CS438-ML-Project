42486816f41ed5629a27b36b27512ad54b7d8227
jimmytran16 <jimmytran1620@gmail.com>
2021-12-06 17:24:01 -0500

    addded tooltip

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ function App() {
     <div>
       <Navbar bg="light">
         <Container>
-          <Navbar.Brand href="#home">Airlines Sentimental Tweets Analysis</Navbar.Brand>
+          <Navbar.Brand>Airlines Sentimental Tweets Analysis</Navbar.Brand>
         </Container>
       </Navbar>
       <div className="graph__wrapper">
@@ -51,7 +51,7 @@ function App() {
                 {
                   (isLoading)
                     ? <SentimentalSpinner />
-                    : <SentimentalBarChart data={data.sentimental.data} options={data.sentimental.optionsBar} />
+                    : <SentimentalBarChart data={data.sentimental.data} options={data.sentimental.optionsBar} toolTipContent={'This is a bar graph that lays out the counts of the sentimentals'} />
                 }
               </Container>
             </div>
@@ -64,7 +64,7 @@ function App() {
                 {
                   (isLoading)
                     ? <SentimentalSpinner />
-                    : <SentimentalBarChart data={data.airlines.data} options={data.airlines.options} />
+                    : <SentimentalBarChart data={data.airlines.data} options={data.airlines.options} toolTipContent={'This is a bar graph that shows the counts of tweets of all the airlines that were mentioned'} />
                 }
               </Container>
             </div>
@@ -75,7 +75,7 @@ function App() {
                 {
                   (isLoading)
                     ? <SentimentalSpinner />
-                    : <SentimentalBarChart data={data.negative_test.data} options={data.negative_test.options} />
+                    : <SentimentalBarChart data={data.negative_test.data} options={data.negative_test.options} toolTipContent={'This is a bar graph shows the different categories of negative reasons and the counts of it'} />
                 }
               </Container>
             </div>
@@ -88,7 +88,7 @@ function App() {
                 {
                   (isLoading)
                     ? <SentimentalSpinner />
-                    : <SentimentalBarChart data={data.null_values_from_columns.data} options={data.null_values_from_columns.options} />
+                    : <SentimentalBarChart data={data.null_values_from_columns.data} options={data.null_values_from_columns.options} toolTipContent={'This is a bar graph shows the counts of the null values that existed within each of the data sets'} />
                 }
               </Container>
             </div>
