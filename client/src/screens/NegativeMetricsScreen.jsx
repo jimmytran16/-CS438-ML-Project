0d55d75b370f32ad4b64be7148d3e69fd8fcde71
jimmytran16 <jimmytran1620@gmail.com>
2021-12-08 13:05:34 -0500

    finished with negative reasons screen

--- client/src/screens/NegativeMetricsScreen.jsx
+++ client/src/screens/NegativeMetricsScreen.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import SentimentalDoughnutChart from '../components/SentimentalDoughnutChart';
 import SentimentalBarChart from '../components/SentimentalBarChart';
 import SentimentalSpinner from '../components/SentimentalSpinner';
-import { Container, Navbar, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios'
 
-export default function SentimentalMetricsScreen() {
+export default function NegativeMetricsScreen() {
     const [data, setData] = useState([])
     const [isLoading, setIsLoading] = useState(true)
   
     useEffect(() => {
       setTimeout(() => {
-        axios.get(`https://enigmatic-hollows-91721.herokuapp.com/`)
-        // axios.get(`http://localhost:5000/`)
+        axios.get(`https://enigmatic-hollows-91721.herokuapp.com/negatives`)
+        // axios.get(`http://localhost:5000/negatives`)
           .then(res => {
             console.log(res.data)
             setData(res.data)
@@ -27,35 +26,11 @@ export default function SentimentalMetricsScreen() {
         <Row>
           <Col xs={12} md={6}>
             <div className="box__wrapper">
-              <Container style={{ minHeight: '100%' }}>
+              <Container style={{ minHeight: '80%' }}>
                 {
                   (isLoading)
                     ? <SentimentalSpinner />
-                    : <SentimentalDoughnutChart data={data.sentimental.data} options={data.sentimental.optionsDoughnut} toolTipContent={'This is a doughnut graph that shows the sentimentals of all the tweets'} />
-                }
-              </Container>
-            </div>  
-          </Col>
-          <Col xs={12} md={6}>
-            <div className="box__wrapper">
-              <Container style={{ minHeight: '100%' }}>
-                {
-                  (isLoading)
-                    ? <SentimentalSpinner />
-                    : <SentimentalBarChart data={data.airlines.data} options={data.airlines.options} toolTipContent={'This is a bar graph that shows the counts of tweets of all the airlines that were mentioned'} />
-                }
-              </Container>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-        <Col>
-            <div className="box__wrapper">
-              <Container style={{ minHeight: '100%' }}>
-                {
-                  (isLoading)
-                    ? <SentimentalSpinner />
-                    : <SentimentalBarChart data={data.accuracy.data} options={data.accuracy.options} toolTipContent={'This is a bar graph shows the accuracy of each of the Machine Learning Models'} />
+                    : <SentimentalBarChart data={data.american.data} options={data.american.options} toolTipContent={'Shows the count of negative reasons per category for American Airlines'} />
                 }
               </Container>
             </div>
@@ -66,20 +41,55 @@ export default function SentimentalMetricsScreen() {
                 {
                   (isLoading)
                     ? <SentimentalSpinner />
-                    : <SentimentalBarChart data={data.negative_test.data} options={data.negative_test.options} toolTipContent={'This is a bar graph shows the different categories of negative reasons and the counts of it'} />
+                    : <SentimentalBarChart data={data.delta.data} options={data.delta.options} toolTipContent={'Shows the count of negative reasons per category for Delta Airlines'} />
                 }
               </Container>
             </div>
           </Col>
         </Row>
         <Row>
-          <Col>
+        <Col xs={12} md={6}>
             <div className="box__wrapper">
               <Container style={{ minHeight: '100%' }}>
                 {
                   (isLoading)
                     ? <SentimentalSpinner />
-                    : <SentimentalBarChart data={data.null_values_from_columns.data} options={data.null_values_from_columns.options} toolTipContent={'This is a bar graph shows the counts of the null values that existed within each of the data sets'} />
+                    : <SentimentalBarChart data={data.southwest.data} options={data.southwest.options} toolTipContent={'Shows the count of negative reasons per category for Southwest Airlines'} />
+                }
+              </Container>
+            </div>
+          </Col>
+          <Col xs={12} md={6}>
+            <div className="box__wrapper">
+              <Container style={{ minHeight: '100%' }}>
+                {
+                  (isLoading)
+                    ? <SentimentalSpinner />
+                    : <SentimentalBarChart data={data.united.data} options={data.united.options} toolTipContent={'Shows the count of negative reasons per category for United Airlines'} />
+                }
+              </Container>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <div className="box__wrapper">
+              <Container style={{ minHeight: '100%' }}>
+                {
+                  (isLoading)
+                    ? <SentimentalSpinner />
+                    : <SentimentalBarChart data={data.us_airways.data} options={data.us_airways.options} toolTipContent={'Shows the count of negative reasons per category for US Airways Airlines'} />
+                }
+              </Container>
+            </div>
+          </Col>
+          <Col xs={12} md={6}>
+            <div className="box__wrapper">
+              <Container style={{ minHeight: '100%' }}>
+                {
+                  (isLoading)
+                    ? <SentimentalSpinner />
+                    : <SentimentalBarChart data={data.virgin_america.data} options={data.virgin_america.options} toolTipContent={'Shows the count of negative reasons per category for Virgin America Airlines'} />
                 }
               </Container>
             </div>
--- server/app.py
+++ server/app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
+# endpoint for Sentimental Analysis
 class SentimentalAnalysis(Resource):
     def __init__(self):
         self.loader = Loader()
@@ -20,8 +21,26 @@ class SentimentalAnalysis(Resource):
             "accuracy": self.loader.get_accuracy_for_models()
         }
         return jsonify(data)
+    
+# endpoint for negative reasons analysis
+class NegativesReasonsAnalysis(Resource):
+    def __init__(self):
+        self.loader = Loader()
+
+    def get(self):
+        data = {
+            "virgin_america": self.loader.get_negative_reason_count_per_airline("Virgin America"),
+            "united": self.loader.get_negative_reason_count_per_airline("United"),
+            "southwest": self.loader.get_negative_reason_count_per_airline("Southwest"),
+            "delta": self.loader.get_negative_reason_count_per_airline("Delta"),
+            "us_airways": self.loader.get_negative_reason_count_per_airline("US Airways"),
+            "american": self.loader.get_negative_reason_count_per_airline("American"),
+        }
+
+        return jsonify(data)
 
 api.add_resource(SentimentalAnalysis, '/')
+api.add_resource(NegativesReasonsAnalysis, '/negatives')
 
 if __name__ == '__main__':
     app.run(debug=True)
