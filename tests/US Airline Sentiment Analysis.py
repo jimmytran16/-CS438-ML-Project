9525ec6f9ae5d226cf4b2151396ffe479f24cbe8
Shreyansh Dhandhukia <shreyansh@Shreyanshs-MacBook-Air.local>
2021-12-12 18:52:28 -0500

    learning

--- tests/US Airline Sentiment Analysis.py
+++ tests/US Airline Sentiment Analysis.py
@@ -7,18 +7,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 import re
 
+from sklearn.model_selection import learning_curve
 
 from sklearn.model_selection import train_test_split # Split Data 
 from imblearn.over_sampling import SMOTE # Handling Imbalanced
 
 # Model Building
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 
 
 from sklearn.metrics import classification_report , confusion_matrix , accuracy_score # Performance Metrics  
-
+from sklearn.metrics import roc_curve, auc
 
 # Data Visualization 
 import matplotlib.pyplot as plt
@@ -40,8 +41,8 @@ def convert_Sentiment(sentiment):
 '''
     Remove URLs
 '''
-def remove_URLs(tweetData):
-    url = re.complie(r'https?://\S+|www\.\S+')
+def remove_url(tweetData):
+    url = re.compile(r'https?://\S+|www\.\S+')
     return url.sub(r'',tweetData)
 
 '''
@@ -90,7 +91,7 @@ def remove_emoji(text):
 '''
     Decontraction of the tweets
 '''
-def remove_emoji(text):
+def decontraction(text):
     text = re.sub(r"won\'t", " will not", text)
     text = re.sub(r"won\'t've", " will not have", text)
     text = re.sub(r"can\'t", " can not", text)
@@ -120,7 +121,7 @@ def remove_emoji(text):
 '''
    Seperate alphanumberic textual data
 '''
-def seperate_alpha_num(text):
+def seperate_alphanumeric(text):
     temp = text
     words = re.findall(r"[^\W\d_]+|\d+", temp)
     return " ".join(words)
@@ -178,4 +179,241 @@ negative = df[df['airline_sentiment'] == 'negative'].text
 
 df.airline_sentiment = df.airline_sentiment.apply(lambda x : convert_Sentiment(x))
 print(df.airline_sentiment)
+
+
+# combaine negative reason with  tweet (if exsist)
+df['final_text'] = df['negativereason'].fillna('') + ' ' + df['text'] 
+
+
+# Apply functions on tweets
+df['final_text'] = df['final_text'].apply(lambda x : remove_username(x))
+df['final_text'] = df['final_text'].apply(lambda x : remove_url(x))
+df['final_text'] = df['final_text'].apply(lambda x : remove_emoji(x))
+df['final_text'] = df['final_text'].apply(lambda x : decontraction(x))
+df['final_text'] = df['final_text'].apply(lambda x : seperate_alphanumeric(x))
+df['final_text'] = df['final_text'].apply(lambda x : unique_char(cont_rep_char,x))
+df['final_text'] = df['final_text'].apply(lambda x : char(x))
+df['final_text'] = df['final_text'].apply(lambda x : x.lower())
+df['final_text'] = df['final_text'].apply(lambda x : remove_stopwords(x))
+
+X = df['final_text']
+y = df['airline_sentiment']
+
+n_classes = 3
+
+# Apply TFIDF on cleaned tweets
+tfid = TfidfVectorizer()
+X_final =  tfid.fit_transform(X)
+
+smote = SMOTE()
+x_sm,y_sm = smote.fit_resample(X_final,y)
+
+# Split Data into train & test 
+X_train , X_test , y_train , y_test = train_test_split(x_sm , y_sm , test_size=0.2)
+X_train , X_val , y_train , y_val = train_test_split(X_train , y_train , test_size=0.25)
+
+LR = LogisticRegression(penalty = 'l2', C = 500,random_state = 0)
+LR.fit(X_train,y_train)
+LR_prediction =  LR.predict(X_val)
+print(accuracy_score(LR_prediction,y_val))
+
+def plot_learning_curve(
+    estimator,
+    title,
+    X,
+    y,
+    axes=None,
+    ylim=None,
+    cv=None,
+    n_jobs=None,
+    train_sizes=np.linspace(0.1, 1.0, 5),
+):
+    """
+    Generate 3 plots: the test and training learning curve, the training
+    samples vs fit times curve, the fit times vs score curve.
+
+    Parameters
+    ----------
+    estimator : estimator instance
+        An estimator instance implementing `fit` and `predict` methods which
+        will be cloned for each validation.
+
+    title : str
+        Title for the chart.
+
+    X : array-like of shape (n_samples, n_features)
+        Training vector, where ``n_samples`` is the number of samples and
+        ``n_features`` is the number of features.
+
+    y : array-like of shape (n_samples) or (n_samples, n_features)
+        Target relative to ``X`` for classification or regression;
+        None for unsupervised learning.
+
+    axes : array-like of shape (3,), default=None
+        Axes to use for plotting the curves.
+
+    ylim : tuple of shape (2,), default=None
+        Defines minimum and maximum y-values plotted, e.g. (ymin, ymax).
+
+    cv : int, cross-validation generator or an iterable, default=None
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+
+          - None, to use the default 5-fold cross-validation,
+          - integer, to specify the number of folds.
+          - :term:`CV splitter`,
+          - An iterable yielding (train, test) splits as arrays of indices.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : int or None, default=None
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+
+    train_sizes : array-like of shape (n_ticks,)
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the ``dtype`` is float, it is regarded
+        as a fraction of the maximum size of the training set (that is
+        determined by the selected validation method), i.e. it has to be within
+        (0, 1]. Otherwise it is interpreted as absolute sizes of the training
+        sets. Note that for classification the number of samples usually have
+        to be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+    if axes is None:
+        _, axes = plt.subplots(1, 3, figsize=(20, 5))
+
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(
+        estimator,
+        X,
+        y,
+        cv=cv,
+        n_jobs=n_jobs,
+        train_sizes=train_sizes,
+        return_times=True,
+    )
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+
+    # Plot learning curve
+    axes[0].grid()
+    axes[0].fill_between(
+        train_sizes,
+        train_scores_mean - train_scores_std,
+        train_scores_mean + train_scores_std,
+        alpha=0.1,
+        color="r",
+    )
+    axes[0].fill_between(
+        train_sizes,
+        test_scores_mean - test_scores_std,
+        test_scores_mean + test_scores_std,
+        alpha=0.1,
+        color="g",
+    )
+    axes[0].plot(
+        train_sizes, train_scores_mean, "o-", color="r", label="Training score"
+    )
+    axes[0].plot(
+        train_sizes, test_scores_mean, "o-", color="g", label="Cross-validation score"
+    )
+    axes[0].legend(loc="best")
+
+    # Plot n_samples vs fit_times
+    axes[1].grid()
+    axes[1].plot(train_sizes, fit_times_mean, "o-")
+    axes[1].fill_between(
+        train_sizes,
+        fit_times_mean - fit_times_std,
+        fit_times_mean + fit_times_std,
+        alpha=0.1,
+    )
+    axes[1].set_xlabel("Training examples")
+    axes[1].set_ylabel("fit_times")
+    axes[1].set_title("Scalability of the model")
+
+    # Plot fit_time vs score
+    axes[2].grid()
+    axes[2].plot(fit_times_mean, test_scores_mean, "o-")
+    axes[2].fill_between(
+        fit_times_mean,
+        test_scores_mean - test_scores_std,
+        test_scores_mean + test_scores_std,
+        alpha=0.1,
+    )
+    axes[2].set_xlabel("fit_times")
+    axes[2].set_ylabel("Score")
+    axes[2].set_title("Performance of the model")
+
+    return plt
+
+title = r"Learning Curves (Logistic Regression)"
+fig, axes = plt.subplots(3, 2, figsize=(10, 15))
+plot_learning_curve(LogisticRegression(penalty = 'l2', C = 500,random_state = 0),title, x_sm, y_sm, axes=axes[:, 1], ylim=(0.7, 1.01), cv=5, n_jobs=4)
+plt.show()
+
+'''
+   Visualize model performance
+'''
+cr = classification_report(y_val, LR_prediction)
+print("Classification Report Logistic Regression:\n----------------------\n", cr)
+cm = confusion_matrix(y_test,LR_prediction)
+
+##'''
+##   Plot confusion matrix
+##'''
+##plt.figure(figsize=(8,6))
+##sentiment_classes = ['Negative', 'Neutral', 'Positive']
+##sns.heatmap(cm, cmap=plt.cm.Blues, annot=True, fmt='d', 
+##            xticklabels=sentiment_classes,
+##            yticklabels=sentiment_classes)
+##plt.title('Confusion matrix', fontsize=16)
+##plt.xlabel('Actual label', fontsize=12)
+##plt.ylabel('Predicted label', fontsize=12)
+##plt.show()
+
+##svm = SVC()
+##svm.fit(X_train,y_train)
+##svm_prediction =  svm.predict(X_test)
+##print(accuracy_score(svm_prediction,y_test))
+
+##
+##'''
+##   Visualize model performance
+##'''
+##cr = classification_report(y_test, svm_prediction)
+##print("Classification Report SVM:\n----------------------\n", cr)
+##cm = confusion_matrix(y_test,svm_prediction)
+##
+##
+##'''
+##   Plot confusion matrix
+##'''
+##plt.figure(figsize=(8,6))
+##sentiment_classes = ['Negative', 'Neutral', 'Positive']
+##sns.heatmap(cm, cmap=plt.cm.Blues, annot=True, fmt='d', 
+##            xticklabels=sentiment_classes,
+##            yticklabels=sentiment_classes)
+##plt.title('Confusion matrix', fontsize=16)
+##plt.xlabel('Actual label', fontsize=12)
+##plt.ylabel('Predicted label', fontsize=12)
+##plt.show()
+
 
